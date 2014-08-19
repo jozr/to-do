c8e47c811e0638ba8049725ee91cb4a6d4065703
Sean Niesen & Josephine Wright <student@epicodus.com>
2014-08-19 16:48:38 -0700

    deletes tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,4 +24,24 @@ class TasksController < ApplicationController
     @task = Task.new
     render('tasks/new.html.erb')
   end
+
+  def edit
+    @task = Task.find(params[:id])
+    render('tasks/edit.html.erb')
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(:name => params[:name])
+      render('tasks/success.html.erb')
+    else
+      render('tasks/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    render('tasks/destroy.html.erb')
+  end
 end
